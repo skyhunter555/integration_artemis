2918bcabf1f8e5500329567b9cd6775e62d78eba
skyhunter <skyhunter555@gmail.com>
2021-03-15 17:57:42 +0300

    fix config jms TLS

--- src/main/java/ru/syntez/integration/artemis/IntegrationArtemisMain.java
+++ src/main/java/ru/syntez/integration/artemis/IntegrationArtemisMain.java
@@ -1,19 +1,102 @@
 package ru.syntez.integration.artemis;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.apache.activemq.ActiveMQSslConnectionFactory;
+import org.apache.activemq.command.ActiveMQDestination;
+import ru.syntez.integration.artemis.entities.RoutingDocument;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.MessageListener;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+import javax.jms.JMSException;
+import java.util.logging.Logger;
+
 /**
  * Main class for console running
  *
  * @author Skyhunter
- * @date 04.02.2021
+ * @date 15.03.2021
  */
-@SpringBootApplication
+
 public class IntegrationArtemisMain {
 
-    public static void main(String[] args) {
+    private final static Logger LOG = Logger.getLogger(IntegrationArtemisMain.class.getName());
 
-        SpringApplication.run(IntegrationArtemisMain.class, args);
+    private static final String brokerConnector = "ssl://127.0.0.1:61618";
+    private static final String brokerUser = "user";
+    private static final String brokerPass = "user";
+    private static final String queueInputOutputEndpoint = "queue://inputToOutputQueue3";
+    private static final String sslClientStorePass = "user555";
+    private static final String sslClientTrustedStorePath = "C:/Users/skyhunter/client_consumer_ts.p12";
+    private static final String sslClientKeyStorePath = "C:/Users/skyhunter/client_consumer_ks.p12";
+
+    public static void main(String[] args) throws Exception {
+
+        LOG.info("Receive Setup...");
+        ConnectionFactory connectionFactory = getConnectionFactory();
+
+        Destination destination = ActiveMQDestination.createDestination(queueInputOutputEndpoint,  ActiveMQDestination.QUEUE_TYPE);
+
+            Connection conn = connectionFactory.createConnection();
+            Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            MessageConsumer consumer = session.createConsumer(destination);
+            consumer.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message)  {
+
+                    LOG.info("Received Message: ");
+                    ObjectMapper xmlMapper = getXmlMapper();
+                    xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                    final String xmlPayload;
+                    if (message instanceof TextMessage) {
+                        // LOG.debug("Text message with no schema");
+                        try {
+                            xmlPayload = ((TextMessage) message).getText();
+                        } catch (JMSException jex) {
+                            LOG.warning(String.format("Unsupported JMS message type %s", message.getClass()));
+                            return;
+                        }
+                    } else {
+                        LOG.warning(String.format("Unsupported JMS message type %s", message.getClass()));
+                        return;
+                    }
+
+                    try {
+                        RoutingDocument document = xmlMapper.readValue(xmlPayload, RoutingDocument.class);
+                        LOG.info(String.format("CONSUMED MESSAGE, docId: %s docType: %s", document.getDocId(), document.getDocType()));
+                    } catch (Exception e) {
+                        LOG.warning(String.format("Error consume message %s", e.getMessage()));
+                    }
+                }
+            });
+            conn.start();
+            Thread.sleep(100000);
+
+    }
+
+    private static ObjectMapper getXmlMapper() {
+        JacksonXmlModule xmlModule = new JacksonXmlModule();
+        xmlModule.setDefaultUseWrapper(false);
+        return new XmlMapper(xmlModule);
+    }
+
+    private static ConnectionFactory getConnectionFactory() throws Exception {
+        ActiveMQSslConnectionFactory connectionFactory = new ActiveMQSslConnectionFactory(brokerConnector);
+        connectionFactory.setTrustStore(sslClientTrustedStorePath);
+        connectionFactory.setTrustStorePassword(sslClientStorePass);
+        connectionFactory.setKeyStore(sslClientKeyStorePath);
+        connectionFactory.setKeyStorePassword(sslClientStorePass);
+        connectionFactory.setUserName(brokerUser);
+        connectionFactory.setPassword(brokerPass);
+        return connectionFactory;
     }
 
 }
--- src/test/java/ru/syntez/integration/artemis/IntegrationArtemisTest.java
+++ src/test/java/ru/syntez/integration/artemis/IntegrationArtemisTest.java
@@ -1,81 +1,58 @@
 package ru.syntez.integration.artemis;
 
 import org.apache.activemq.ActiveMQSslConnectionFactory;
-import org.junit.Ignore;
+import java.util.logging.Logger;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.connection.CachingConnectionFactory;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.jms.*;
+import javax.jms.ConnectionFactory;
+import javax.jms.Connection;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.MessageProducer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-//@Ignore
+/**
+ *
+ *  @author Skyhunter
+ *  @date 15.03.2021
+ */
 public class IntegrationArtemisTest {
 
-    @Value("${jms.activemq.queues.input-output-endpoint}")
-    private String queueInputOutputEndpoint = "inputToOutputQueue";
+    private final static Logger LOG = Logger.getLogger(IntegrationArtemisTest.class.getName());
 
-    @Configuration
-    public static class Config {
+    private final String brokerConnector = "ssl://127.0.0.1:61618";
+    private final String sslClientStorePass = "user555";
+    private final String sslClientTrustedStorePath = "C:/Users/skyhunter/client_sender_ts.p12";
+    private final String sslClientKeyStorePath = "C:/Users/skyhunter/client_sender_ks.p12";
+    private final String brokerUser = "user";
+    private final String brokerPass = "user";
+    private String queueInputOutputEndpoint = "inputToOutputQueue3";
 
-        @Value("${jms.activemq.brokerUrl}")
-        private String brokerConnector = "tcp://localhost:61616";
-
-        @Value("${server.ssl.client-store-password}")
-        private String sslClientStorePass = "user555";
-
-        @Value("${server.ssl.client-sender-trusted-store-path}")
-        private String sslClientTrustedStorePath = "C:/Users/skyhunter/client_sender_ts.p12";
-
-        @Value("${server.ssl.client-sender-key-store-path}")
-        private String sslClientKeyStorePath = "C:/Users/skyhunter/client_sender_ks.p12";
-
-        @Value("${jms.activemq.user}")
-        private String brokerUser = "user";
-
-        @Value("${jms.activemq.password}")
-        private String brokerPass = "user";
-
-        @Bean
-        public CachingConnectionFactory connectionFactory() throws Exception {
-            ActiveMQSslConnectionFactory connectionFactory = new ActiveMQSslConnectionFactory(brokerConnector);
-            connectionFactory.setTrustStore(sslClientTrustedStorePath);
-            connectionFactory.setTrustStorePassword(sslClientStorePass);
-            connectionFactory.setKeyStore(sslClientKeyStorePath);
-            connectionFactory.setKeyStorePassword(sslClientStorePass);
-            connectionFactory.setUserName(brokerUser);
-            connectionFactory.setPassword(brokerPass);
-            CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(connectionFactory);
-            cachingConnectionFactory.setCacheConsumers(false);
-            cachingConnectionFactory.setSessionCacheSize(20);
-            return cachingConnectionFactory;
-        }
+    private ConnectionFactory getConnectionFactory() throws Exception {
+        ActiveMQSslConnectionFactory connectionFactory = new ActiveMQSslConnectionFactory(brokerConnector);
+        connectionFactory.setTrustStore(sslClientTrustedStorePath);
+        connectionFactory.setTrustStorePassword(sslClientStorePass);
+        connectionFactory.setKeyStore(sslClientKeyStorePath);
+        connectionFactory.setKeyStorePassword(sslClientStorePass);
+        connectionFactory.setUserName(brokerUser);
+        connectionFactory.setPassword(brokerPass);
+        return connectionFactory;
     }
-
-    @Autowired
-    ConnectionFactory connectionFactory;
 
     @Test
     public void sendMessageToInputQueueTest() {
 
         try {
             String messageXml = new String(Files.readAllBytes(Paths.get(getClass().getResource("/router_doc_1.xml").toURI())));
-            Connection connection = connectionFactory.createConnection();
+            Connection connection = getConnectionFactory().createConnection();
             connection.start();
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             MessageProducer msgProducer = session.createProducer(session.createQueue(queueInputOutputEndpoint));
-            for (int i = 0; i < 200; i++) {
+            for (int i = 0; i < 10; i++) {
                 TextMessage textMessage = session.createTextMessage(messageXml);
                 msgProducer.send(textMessage);
+                LOG.info("JMS text message sended to queue");
             }
         } catch (Exception e) {
             e.printStackTrace();
